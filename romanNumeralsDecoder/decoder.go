90a9d0c76fa9515becbbf5299fa4d461a0ea7ff2
iMeisa <iMeisa@github.com>
2021-04-02 15:01:55 +0300

    Added Roman Numerals Decoder

---
+++ romanNumeralsDecoder/decoder.go
@@ -0,0 +1,43 @@
+package romanNumeralsDecoder
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Decode(roman string) int {
+
+	var total int
+	prevValue := 0
+	for _, letterByte := range roman {
+		letter := strings.ToUpper(string(letterByte))
+		var currentValue int
+		switch letter {
+		case "I":
+			currentValue = 1
+		case "V":
+			currentValue = 5
+		case "X":
+			currentValue = 10
+		case "L":
+			currentValue = 50
+		case "C":
+			currentValue = 100
+		case "D":
+			currentValue = 500
+		case "M":
+			currentValue = 1000
+		default:
+			fmt.Printf("Invalid character: %v\n", letter)
+			return 0
+		}
+
+		if prevValue < currentValue {
+			currentValue -= prevValue
+		}
+
+		total += currentValue
+	}
+
+	return total
+}
