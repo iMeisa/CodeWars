5bba36ed444f12cf2f80807ed4eb26e45f3d6203
iMeisa <62470487+iMeisa@users.noreply.github.com>
2022-09-15 14:25:27 +0300

    Refactored

--- tester.py
+++ tester.py
@@ -1,3 +1,3 @@
-from lambdaToDef.lambda_to_def import convert_lambda_to_def
+from python.lambdaToDef.lambda_to_def import convert_lambda_to_def
 
 print(convert_lambda_to_def("act = lambda h: h + ' ' + 'i'"))
